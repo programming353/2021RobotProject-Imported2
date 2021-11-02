ff3b669e524e21b0b336e872a32217219ec3347f
programming353 <80562795+Jason497@users.noreply.github.com>
2021-11-01 20:25:53 -0400

    Update AutonomousDrive.java

--- src/main/java/frc/robot/commands/AutonomousDrive.java
+++ src/main/java/frc/robot/commands/AutonomousDrive.java
@@ -6,13 +6,12 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.commands;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.HoodSubsystem;
-
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 //import frc.robot.subsystems.HoodSubsystem;
 import edu.wpi.first.networktables.*;
